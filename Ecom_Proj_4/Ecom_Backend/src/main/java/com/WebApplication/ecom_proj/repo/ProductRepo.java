b698857c19d13fd1a0e75953adbab849e4c68d64
Herin Patel <herin.patel@volansys.com>
2025-01-16 23:25:57 +0530

    Added till search functionality

---
+++ Ecom_Proj_4/Ecom_Backend/src/main/java/com/WebApplication/ecom_proj/repo/ProductRepo.java
@@ -0,0 +1,12 @@
+package com.WebApplication.ecom_proj.repo;
+
+import com.WebApplication.ecom_proj.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepo extends JpaRepository<Product, Integer> {
+}
---
+++ Ecom_Proj_4/Ecom_Backend/src/main/java/com/WebApplication/ecom_proj/service/ProductService.java
@@ -0,0 +1,43 @@
+package com.WebApplication.ecom_proj.service;
+
+import com.WebApplication.ecom_proj.model.Product;
+import com.WebApplication.ecom_proj.repo.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepo repo;
+
+    public List<Product> getAllProducts() {
+        return repo.findAll();
+    }
+
+    public Product getProductById(int id) {
+        return repo.findById(id).orElse(null);
+    }
+
+    public Product addProduct(Product newProduct, MultipartFile imageFile) throws IOException {
+        newProduct.setImageName(imageFile.getOriginalFilename());
+        newProduct.setImageType(imageFile.getContentType());
+        newProduct.setImageData(imageFile.getBytes());
+        return repo.save(newProduct);
+    }
+
+    public Product updateProduct(int id, Product product, MultipartFile imageFile) throws IOException {
+        product.setImageData(imageFile.getBytes());
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        return repo.save(product);
+    }
+
+    public void deleteProduct(int id){
+        repo.deleteById(id);
+    }
+}
