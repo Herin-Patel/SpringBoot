0951c57cfdc5b31da7d0dc5549bd7136dc03ade2
Herin Patel <herin.patel@volansys.com>
2025-01-20 11:23:18 +0530

    Added upto front-end 5 working database

---
+++ Ecom_Proj_New_DB/Ecom_Backend_2/src/main/java/com/WebApplication/simpleEcom/controller/ProductController.java
@@ -0,0 +1,103 @@
+package com.WebApplication.simpleEcom.controller;
+
+import com.WebApplication.simpleEcom.model.Product;
+import com.WebApplication.simpleEcom.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api")
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @RequestMapping("/")
+    public String greet() {
+        return "Hello World";
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getAllProducts() {
+        return new ResponseEntity<>(service.getAllProducts(), HttpStatus.OK);
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable int id) {
+
+        Product productobj = service.getProductById(id);
+
+        if (productobj != null)
+            return new ResponseEntity<>(service.getProductById(id), HttpStatus.OK);
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(
+            @RequestPart Product product,
+            @RequestPart MultipartFile imageFile) {
+        try {
+            System.out.println(product);
+            Product savedProduct = service.addProduct(product, imageFile);
+            return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
+        } catch (Exception expobj) {
+            return new ResponseEntity<>(expobj.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/product/{productId}/image")
+    public ResponseEntity<byte[]> getImageByProductId(@PathVariable int productId) {
+        Product currentProd = service.getProductById(productId);
+
+        byte[] imageFile = currentProd.getImageData();
+
+        return ResponseEntity.ok().
+                contentType(MediaType.valueOf(currentProd.getImageType()))
+                .body(imageFile);
+    }
+
+    @PutMapping("/product/{id}")
+    public ResponseEntity<String> updateProduct(@PathVariable int id,
+                                                @RequestPart Product product,
+                                                @RequestPart MultipartFile imageFile) {
+        Product updatedProduct = null;
+        try {
+            updatedProduct = service.updateProduct(id, product, imageFile);
+        } catch (IOException expobj) {
+            return new ResponseEntity<>("Failed to update", HttpStatus.BAD_REQUEST);
+        }
+
+        if (updatedProduct != null)
+            return new ResponseEntity<>("Updated", HttpStatus.OK);
+        else
+            return new ResponseEntity<>("Failed to update", HttpStatus.BAD_REQUEST);
+    }
+
+    @DeleteMapping("/product/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable int id) {
+        Product deleteProduct = service.getProductById(id);
+
+        if (deleteProduct != null) {
+            service.deleteProduct(id);
+            return new ResponseEntity<>("Deleted", HttpStatus.OK);
+        } else
+            return new ResponseEntity<>("Product not found", HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/products/search")
+    public ResponseEntity<List<Product>> searchProducts(@RequestParam String keyword) {
+        System.out.println("Searching with " + keyword);
+        List<Product> searchProducts = service.searchProducts(keyword);
+
+        return new ResponseEntity<>(searchProducts, HttpStatus.OK);
+    }
+}
---
+++ Ecom_Proj_New_DB/Ecom_Backend_2/src/main/java/com/WebApplication/simpleEcom/service/ProductService.java
@@ -0,0 +1,49 @@
+package com.WebApplication.simpleEcom.service;
+
+import com.WebApplication.simpleEcom.model.Product;
+import com.WebApplication.simpleEcom.repo.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepo repo;
+
+    public List<Product> getAllProducts() {
+        return repo.findAll();
+    }
+
+    public Product getProductById(int id) {
+        return repo.findById(id).orElse(null);
+    }
+
+    public Product addProduct(Product product, MultipartFile imageFile) throws IOException {
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageData(imageFile.getBytes());
+
+        return repo.save(product);
+    }
+
+    public Product updateProduct(int id, Product product, MultipartFile imageFile) throws IOException {
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageData(imageFile.getBytes());
+
+        return repo.save(product);
+    }
+
+    public void deleteProduct(int id) {
+        repo.deleteById(id);
+    }
+
+    public List<Product> searchProducts(String keyword) {
+        return repo.searchProducts(keyword);
+    }
+}
